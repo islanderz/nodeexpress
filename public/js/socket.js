ae6b3154b1dbd1a09e40c6674900b2f53ccfcb0e
islanderz <kamil.saib@accenture.com>
2015-09-30 09:01:21 +0200

    updates

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,36 +1,32 @@
-
 //$.getScript("js/mqttws31.js", function(){
 //   alert("Script loaded but not necessarily executed.");
 //});
 
-
 function drawImage(imgData, coords) {
-    "use strict";
-	 var canvasImage = document.getElementById("thecanvas");
-	 var ctx = canvasImage.getContext("2d");
-	 
-    //var uInt8Array = new Uint8Array(imgData);
-    var uInt8Array = imgData;
-    var i = uInt8Array.length;
-    var binaryString = [i];
-    while (i--) {
-        binaryString[i] = String.fromCharCode(uInt8Array[i]);
-    }
-    var data = binaryString.join('');
+	"use strict";
+	var canvasImage = document.getElementById("thecanvas");
+	var ctx = canvasImage.getContext("2d");
 
-    var base64 = window.btoa(data);
+	// var uInt8Array = new Uint8Array(imgData);
+	var uInt8Array = imgData;
+	var i = uInt8Array.length;
+	var binaryString = [ i ];
+	while (i--) {
+		binaryString[i] = String.fromCharCode(uInt8Array[i]);
+	}
+	var data = binaryString.join('');
 
-    var img = new Image();
-    img.src = "data:image/png;base64," + base64;
-    img.onload = function () {
-        console.log("Image Onload"); 
-        ctx.drawImage(img, coords[0], coords[1], canvasImage.width, canvasImage.height);
-    };
-    img.onerror = function (stuff) {
-        console.log("Img Onerror:", stuff);
-    };
- 
+	var base64 = window.btoa(data);
+
+	var img = new Image();
+	img.src = "data:image/png;base64," + base64;
+	img.onload = function() {
+		console.log("Image Onload");
+		ctx.drawImage(img, coords[0], coords[1], canvasImage.width,
+				canvasImage.height);
+	};
+	img.onerror = function(stuff) {
+		console.log("Img Onerror:", stuff);
+	};
+
 }
-
-
-
